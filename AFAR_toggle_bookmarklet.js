f63bae572aec9db44e7aad658075c8d879ee6e5b
theinternauts <cricketw@wearetheinternauts.com>
2015-05-02 23:15:35 -0700

    minor tweak to stop the current page load before attempting to load the new page

--- AFAR_toggle_bookmarklet.js
+++ AFAR_toggle_bookmarklet.js
@@ -2,6 +2,7 @@ javascript: (function(window) {
   var DOMAIN_LIST = ['l.afar.com:3000','staging.afar.com','staging2.afar.com','www.afar.com'];
   var newIndex = DOMAIN_LIST.indexOf(window.location.host)
   if (newIndex >= 0){
+    window.stop();
     newD = DOMAIN_LIST[(newIndex+1) % DOMAIN_LIST.length]
     window.location.href = window.location.href.replace(window.location.host, newD)
   }else{
