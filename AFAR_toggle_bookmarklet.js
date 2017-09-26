9f61fc34793580d43bfba87fadfa88be3a0bd10d
theinternauts <cricketw@wearetheinternauts.com>
2017-09-26 11:34:30 -0700

    better variable names

--- AFAR_toggle_bookmarklet.js
+++ AFAR_toggle_bookmarklet.js
@@ -1,11 +1,11 @@
 javascript: (function(window) {
-  var DOMAIN_LIST = ['l.afar.com:3000','staging.afar.com','staging2.afar.com','www.afar.com'];
-  var newIndex = DOMAIN_LIST.indexOf(window.location.host)
-  if (newIndex >= 0){
+  var HOST_LIST = ['l.afar.com:3000','staging.afar.com','staging2.afar.com','www.afar.com'];
+  var initialHostIndex = HOST_LIST.indexOf(window.location.host)
+  if (initialHostIndex >= 0){
     window.stop();
-    newD = DOMAIN_LIST[(newIndex+1) % DOMAIN_LIST.length]
-    window.location.href = window.location.href.replace('https://', 'http://').replace(window.location.host, newD);
+    newHost = HOST_LIST[(initialHostIndex+1) % HOST_LIST.length]
+    window.location.href = window.location.href.replace('https://', 'http://').replace(window.location.host, newHost);
   }else{
-    console.log("You aren't starting on one of the AFAR URLs: ", DOMAIN_LIST.toString())
+    console.log("You aren't starting on one of the AFAR URLs: ", HOST_LIST.toString())
   }
 })(window);
