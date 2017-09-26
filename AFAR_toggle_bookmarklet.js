f7544b98b2124218aceaf70a2643a138563b4067
theinternauts <cricketw@wearetheinternauts.com>
2017-09-26 11:31:41 -0700

    ensure new location does not use ssl (for local build compatabilty)

--- AFAR_toggle_bookmarklet.js
+++ AFAR_toggle_bookmarklet.js
@@ -4,7 +4,7 @@ javascript: (function(window) {
   if (newIndex >= 0){
     window.stop();
     newD = DOMAIN_LIST[(newIndex+1) % DOMAIN_LIST.length]
-    window.location.href = window.location.href.replace(window.location.host, newD)
+    window.location.href = window.location.href.replace('https://', 'http://').replace(window.location.host, newD);
   }else{
     console.log("You aren't starting on one of the AFAR URLs: ", DOMAIN_LIST.toString())
   }
